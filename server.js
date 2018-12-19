1f0645ecf3ba8c2d7452dfab506b833f020b22c8
Jonathan Smith <jonathan@ool.co.uk>
2018-12-19 16:20:25 +0000

    after staging the changes to server.js Signed-off-by: Jonathan Smith <jonathan@ool.co.uk>

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 var http = require('http');
+var express = require('express');
+var url = require('url');
+var cors = require('cors');
+    var bodyParser = require('body-parser');
+    var Connection = require('tedious').Connection;  
+    var Request = require('tedious').Request;  
+    var TYPES = require('tedious').TYPES;  
+
+    var config = {  
+        userName: 'cm97@uobe7kufo3',  
+        password: 'Azur3Pswd',  
+        server: 'uobe7kufo3.database.windows.net',  
+        // If you are on Microsoft Azure, you need this:  
+        options: {encrypt: true, database: 'Teledata', rowCollectionOnRequestCompletion: true}  
+    };  
+    
+//var sql = require("mssql");
+var app = express();
 
 http.createServer(function (req, res) {
     
