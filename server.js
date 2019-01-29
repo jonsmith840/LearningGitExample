26132488a2c5ab7c5cf5ba8c371388db237abafc
Jonathan Smith <jonathan@ool.co.uk>
2019-01-29 18:30:34 +0000

    Not using identity

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-		var identity = req.header('X-MS-CLIENT-PRINCIPAL-ID');
+		var identity = req.header('X-MS-CLIENT-PRINCIPAL-NAME');
 		
 		var requestString = JSON.stringify(req.body);
 			requestObj = JSON.parse(requestString);
@@ -61,7 +61,7 @@ app.post('/', function (req, res) {
         });
         
     });*/
-    res.end(identity);
+    res.end('jonsmith840');
     
 }).listen(process.env.PORT || 8080);
 /*
