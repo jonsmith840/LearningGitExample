df3909f69fd1eafd454a0f823232ee7305280121
Jonathan Smith <jonathan@ool.co.uk>
2019-03-14 16:08:36 +0000

    id and logout

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ var cors = require('cors');
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-		var identity = req.header('x-ms-client-principal-name');
+		var identity = req.header('x-ms-client-principal-id');
 		//var user = req.headers['authorization'].tostring();
 			//var QueryString = 'InvoiceCustomerGoogleSP';
 
-		
+
 		var requestString = JSON.stringify(req.body);
 			var requestObj = JSON.parse(requestString);
 			var QueryString = requestObj.QueryString;
@@ -28,14 +28,14 @@ app.post('/', function (req, res) {
 			sqlParams = sqlParams.slice(1);
     	//var sqlstring = "Exec " + QueryString + sqlParams;
     	//var sqlstring = "Exec InvoiceCustomerGoogleSP, @Google 'jonsmith840@gmail.com'";
-   	
+
     	var sql = require("mssql");
-    	//var TYPES = require('tedious').TYPES;  
+    	//var TYPES = require('tedious').TYPES;
     sql.connect(configString, function (err) {
-    
+
         if (err){
         	console.log(err);
-        } 
+        }
 		var request = new sql.Request();
 		request.input('AuthID', sql.VarChar, identity);
 			for (x in requestObj.Params){
@@ -59,46 +59,46 @@ app.post('/', function (req, res) {
             res.end(JSON.stringify(result.recordset));
             sql.close();
         });
-        
+
     });
     //res.end(identity + " nothing is what's coming through");
     //res.end(JSON.stringify(req.headers));
-    
+
 }).listen(process.env.PORT || 8080);
 /*
 app.get('/', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end("Hello, world. I can't believe mssql worked or why tedious doesn't! Connecting throws an internal error.");
-   
+
     var sql = require("mssql");
 
     // config for your database
     var config = {
         user: 'cm97@uobe7kufo3',
         password: 'Azur3Pswd',
-        server: 'uobe7kufo3.database.windows.net', 
-        database: 'Teledata' 
+        server: 'uobe7kufo3.database.windows.net',
+        database: 'Teledata'
     };
 
     // connect to your database
 
     sql.connect(config, function (err) {
-    
+
         if (err) res.send(err);
 
         // create Request object
 		var request = new sql.Request();
-           
+
         // query to the database and get the records
         request.query('select * from Contacts where Code = 18985', function (err, recordset) {
-            
+
             if (err) console.log(err)
 
             // send records as a response
             res.send(recordset);
         });
     });
-            
+
 });
 
 var server = app.listen(8080, function () {
